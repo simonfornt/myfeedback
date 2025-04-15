d9b8005fb7d12592513f71ea3807ea3716cf0ae8
simonfotnt <simonpiar98@gamil.com>
2025-04-15 21:21:13 +0600

    stepup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <h1 className='text-2xl text-center'> Start MyFeedback assignment</h1>
+    </>
+  )
+}
+
+export default App
